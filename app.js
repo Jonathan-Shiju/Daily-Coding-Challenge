c7f7eac8528bd335dc5a1960b631baedbe25e284
Jonathan Shiju <jonathanshiju2003@gmail.com>
2025-09-12 15:49:00 +0530

    Added .js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import { Strategy as LocalStrategy } from 'passport-local'
 import { fileURLToPath } from 'url'
 
 // Import the new connection function
-import { connectToDatabase } from './config/db'
+import { connectToDatabase } from './config/db.js'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
